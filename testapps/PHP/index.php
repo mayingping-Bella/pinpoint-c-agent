7e5a0430039e4a5648899fb0dc4900b0970c43fb
ma.yingping <13551142978@163.com>
2024-07-12 09:19:47 +0800

    test

--- testapps/PHP/index.php
+++ testapps/PHP/index.php
@@ -9,7 +9,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 
 // A writable path for caching AOP code
-define('APPLICATION_NAME', 'cd.dev.test.php'); // your application name
+define('APPLICATION_NAME', 'cd.dev.test.php.test'); // your application name
 define('APPLICATION_ID', 'cd.dev.test.yii');  // your application id
 define('PP_REQ_PLUGINS', \Pinpoint\Plugins\Yii2PerRequestPlugins::class);
 require_once __DIR__ . '/../vendor/pinpoint-apm/pinpoint-php-aop/auto_pinpointed.php';
